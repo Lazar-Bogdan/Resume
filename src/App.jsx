617e14958c123e11f7bc77504876a986b7d4de94
Lazar-Bogdan <70701594+Lazar-Bogdan@users.noreply.github.com>
2023-10-24 17:12:26 +0000

    mock workExperience

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './normalize.css'
 import Layout from './components/Layout';
 import Details from './components/Details';
+import WorkExperience from './components/WorkExperience';
 
 export default function App() {
   return (
     <Layout
       left={
-        <Details />
+        <div>
+          <Details />
+          <WorkExperience />
+        </div>
       }
       right={
         <div>right side</div>
